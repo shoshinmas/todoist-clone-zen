489b84db0c1c41b04dd7dc8137a1d44026274101
BobbieDraper <bobbiedraper@rocinante.home>
2022-06-01 23:30:01 +0200

    Added user context

--- src/Controller/UserAccess/TaskItemCrudController.php
+++ src/Controller/UserAccess/TaskItemCrudController.php
@@ -3,7 +3,15 @@
 namespace App\Controller\UserAccess;
 
 use App\Entity\TaskItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 
 class TaskItemCrudController extends AbstractCrudController
 {
@@ -12,14 +20,39 @@ class TaskItemCrudController extends AbstractCrudController
         return TaskItem::class;
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
+    public function configureCrud(Crud $crud): Crud
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        return $crud
+            ->setEntityLabelInSingular('Task')
+            ->setEntityLabelInPlural('Tasks')
+            ->setSearchFields(['text'])
+            ->setDefaultSort(['date' => 'DESC'])
+        ;
     }
-    */
+
+   /* public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add(EntityFilter::new('id'))
+        ;
+    }
+
+    public function configureFields(string $pageName): iterable
+     {
+        yield AssociationField::new('id');
+        yield TextareaField::new('text')
+            ->hideOnIndex()
+        ;
+        
+        $date = DateTimeField::new('date')->setFormTypeOptions([
+            'html5' => true,
+            'years' => range(date('Y'), date('Y') + 5),
+            'widget' => 'single_text',
+        ]);
+        if (Crud::PAGE_EDIT === $pageName) {
+            yield $date->setFormTypeOption('disabled', true);
+        } else {
+            yield $date;
+        }
+     }*/
 }
